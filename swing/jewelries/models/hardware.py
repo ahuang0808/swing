3157f50886170bd9140ad224b2f5aabacbaf1f53
Aiden Huang <ahuang9688@gmail.com>
2024-06-19 20:11:26 +0800

    up

--- swing/jewelries/models/hardware.py
+++ swing/jewelries/models/hardware.py
@@ -5,8 +5,6 @@ from django.db.models import ForeignKey
 from django.db.models import Model
 from django.db.models import URLField
 
-from swing.jewelries.models.bead import BeadMaterial
-
 
 class HardwareType(Model):
     """
@@ -36,11 +34,12 @@ class Hardware(Model):
     """
 
     name = CharField(max_length=255)
-    size = DecimalField(max_digits=10, decimal_places=2)
+    length = DecimalField(max_digits=10, decimal_places=2)
+    width = DecimalField(max_digits=10, decimal_places=2)
     type = ForeignKey(HardwareType, on_delete=PROTECT)
     price = DecimalField(max_digits=10, decimal_places=2)
     link = URLField(blank=True, max_length=255)
-    material = ForeignKey(BeadMaterial, on_delete=PROTECT)
+    material = ForeignKey(HardwareMaterial, on_delete=PROTECT)
 
     def __str__(self):
         return self.name
