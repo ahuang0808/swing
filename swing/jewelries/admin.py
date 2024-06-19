199f4c7b07d700e85caf038bb862d6bdee604009
Aiden Huang <ahuang9688@gmail.com>
2024-06-19 20:11:26 +0800

    up

--- swing/jewelries/admin.py
+++ swing/jewelries/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.contrib.admin import TabularInline
 
 from swing.jewelries.models.bead import Bead
 from swing.jewelries.models.bead import BeadMaterial
@@ -7,8 +9,49 @@ from swing.jewelries.models.hardware import HardwareMaterial
 from swing.jewelries.models.hardware import HardwareType
 from swing.jewelries.models.jewelry import Jewelry
 from swing.jewelries.models.jewelry import JewelryType
+from swing.jewelries.models.jewelry import LinkJewelryBead
+from swing.jewelries.models.jewelry import LinkJewelryHardware
+from swing.jewelries.models.jewelry import LinkJewelryJewelryString
 from swing.jewelries.models.jewelry import Series
 from swing.jewelries.models.jewelry_string import JewelryString
+
+
+class LinkJewelryBeadInline(TabularInline):
+    model = LinkJewelryBead
+    extra = 1  # 提供一个额外的空白表单
+
+
+class LinkJewelryHardwareInline(TabularInline):
+    model = LinkJewelryHardware
+    extra = 1
+
+
+class LinkJewelryJewelryStringInline(TabularInline):
+    model = LinkJewelryJewelryString
+    extra = 1
+
+
+@admin.register(Jewelry)
+class JewelryAdmin(ModelAdmin):
+    inlines = [
+        LinkJewelryBeadInline,
+        LinkJewelryHardwareInline,
+        LinkJewelryJewelryStringInline,
+    ]
+    list_display = ("name", "get_length", "get_price")
+
+    @admin.display(
+        description="Length",
+    )
+    def get_length(self, obj):
+        return obj.length()
+
+    @admin.display(
+        description="Price",
+    )
+    def get_price(self, obj):
+        return obj.price()
+
 
 admin.site.register(Bead)
 admin.site.register(BeadMaterial)
@@ -18,4 +61,3 @@ admin.site.register(HardwareType)
 admin.site.register(JewelryString)
 admin.site.register(Series)
 admin.site.register(JewelryType)
-admin.site.register(Jewelry)
--- swing/jewelries/models/jewelry.py
+++ swing/jewelries/models/jewelry.py
@@ -1,12 +1,13 @@
 from django.db.models import PROTECT
+from django.db.models import BooleanField
 from django.db.models import CharField
 from django.db.models import ForeignKey
 from django.db.models import ManyToManyField
 from django.db.models import Model
 from django.db.models import PositiveIntegerField
-from jewelries.models.bead import Bead
-from jewelries.models.hardware import Hardware
 
+from swing.jewelries.models.bead import Bead
+from swing.jewelries.models.hardware import Hardware
 from swing.jewelries.models.jewelry_string import JewelryString
 
 
@@ -42,10 +43,31 @@ class Jewelry(Model):
     type = ForeignKey(JewelryType, on_delete=PROTECT)
     beads = ManyToManyField(Bead, through="LinkJewelryBead")
     hardwares = ManyToManyField(Hardware, through="LinkJewelryHardware")
-    strings = ManyToManyField(JewelryString, through="LinkJewelryString")
+    strings = ManyToManyField(JewelryString, through="LinkJewelryJewelryString")
 
     def __str__(self):
         return self.name
+
+    def length(self):
+        length = 0
+        for linkjewelrybead in self.linkjewelrybead_set.all():
+            length += linkjewelrybead.bead.length * linkjewelrybead.quantity
+        for linkjewelryhardware in self.linkjewelryhardware_set.filter(is_main=True):
+            length += linkjewelryhardware.hardware.length * linkjewelryhardware.quantity
+        return length
+
+    def price(self):
+        price = 0
+        for linkjewelrybead in self.linkjewelrybead_set.all():
+            price += linkjewelrybead.bead.price * linkjewelrybead.quantity
+        for linkjewelryhardware in self.linkjewelryhardware_set.all():
+            price += linkjewelryhardware.hardware.price * linkjewelryhardware.quantity
+        for linkjewelryjewelrystring in self.linkjewelryjewelrystring_set.all():
+            price += (
+                linkjewelryjewelrystring.jewelry_string.price
+                * linkjewelryjewelrystring.quantity
+            )
+        return price
 
 
 class LinkJewelryBead(Model):
@@ -56,6 +78,9 @@ class LinkJewelryBead(Model):
     jewelry = ForeignKey(Jewelry, on_delete=PROTECT)
     bead = ForeignKey(Bead, on_delete=PROTECT)
     quantity = PositiveIntegerField()
+
+    class Meta:
+        unique_together = ["jewelry", "bead"]
 
     def __str__(self):
         return f"{self.jewelry.name} - {self.bead.name} * {self.quantity}"
@@ -69,6 +94,10 @@ class LinkJewelryHardware(Model):
     jewelry = ForeignKey(Jewelry, on_delete=PROTECT)
     hardware = ForeignKey(Hardware, on_delete=PROTECT)
     quantity = PositiveIntegerField()
+    is_main = BooleanField(null=False)  # included in the main body or not
+
+    class Meta:
+        unique_together = ["jewelry", "hardware"]
 
     def __str__(self):
         return f"{self.jewelry.name} - {self.hardware.name} * {self.quantity}"
@@ -80,8 +109,11 @@ class LinkJewelryJewelryString(Model):
     """
 
     jewelry = ForeignKey(Jewelry, on_delete=PROTECT)
-    string = ForeignKey(JewelryString, on_delete=PROTECT)
+    jewelry_string = ForeignKey(JewelryString, on_delete=PROTECT)
     quantity = PositiveIntegerField()
 
+    class Meta:
+        unique_together = ["jewelry", "jewelry_string"]
+
     def __str__(self):
-        return f"{self.jewelry.name} - {self.string.name} * {self.quantity}mm"
+        return f"{self.jewelry.name} - {self.jewelry_string.name} * {self.quantity}mm"
