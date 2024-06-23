8cbc8e138ee6ac89dcb46bc0e1973da2c3d7d611
Aiden Huang <ahuang9688@gmail.com>
2024-06-23 22:12:06 +0800

    Resize image

--- swing/static/js/product_image.js
+++ swing/static/js/product_image.js
@@ -1,3 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var images = document.querySelectorAll('.product-image');
+    images.forEach(function(img) {
+        img.onload = function() {
+            var tempImg = new Image();
+            tempImg.src = img.src;
+            tempImg.onload = function() {
+                var originalWidth = tempImg.width;
+                var originalHeight = tempImg.height;
+
+                if (originalWidth !== originalHeight) {
+                    img.style.width = 'auto';
+                    img.style.height = '150px';
+                } else {
+                    img.style.width = '150px';
+                    img.style.height = '150px';
+                }
+            };
+        };
+    });
+});
+
 function showImageModal(imageSrc) {
     var modal = document.getElementById('image-modal');
     var modalImg = document.getElementById('modal-image');
