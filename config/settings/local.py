1497d87d63442904d2293691710c4d6f8ead33d4
Aiden Huang <ahuang9688@gmail.com>
2024-06-24 22:47:22 +0800

    Add allowed host

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,7 +14,7 @@ SECRET_KEY = env(
     default="OmUUUETSPcHSJrgRbsrKdRUBOHOjgNzPQnAmxttMaSnw4aGBGYvzEfABdEKTolVq",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "aidens-mac-mini.local"]  # noqa: S104
 
 # CACHES
 # ------------------------------------------------------------------------------
